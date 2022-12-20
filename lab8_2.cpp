664027f4064facb6f45f016c65a09d59d8a4df48
PunnawichMai <punnawich_dachin@cmu.ac.th>
2022-12-20 15:41:08 +0700

    lab 8.2 completed

--- lab8_2.cpp
+++ lab8_2.cpp
@@ -1,18 +1,39 @@
-//Dialog of the first example is given below.
+#include <iostream>
+#include <string>
 
-/*
-Fahsai: Sawadee ka...Can you tell me your name?
-?????: Luke Skywalker
-Fahsai: Wow!!! Luke Skywalker is a really cool name.
-Fahsai: I think you are an Engineering student. What is your student ID?
-Luke Skywalker: 590610999
-Fahsai: I think you may be GEAR 47. I have a free movie ticket for you.
-Fahsai: Let's go to the cinema together!!!
-Fahsai: What movie do you want to watch?
-Luke Skywalker: Star Wars VII
-Fahsai: So....which day are you free to go with me?
-Luke Skywalker: Next Monday
-Fahsai: Next Monday....that is OK!!! I'm looking forward to watching Star Wars VII with you.
-Luke Skywalker: May the Force be with you krub
-Fahsai: 555+ see you Next Monday. Bye Bye \(^ ^)/
-*/
+using namespace std;
+
+int main()
+{
+    string name;
+    cout << "Fahsai: Sawadee ka...Can you tell me your name?\n";
+    cout << "?????: " ;
+    getline(cin,name);
+
+    cout << "Fahsai: Wow!!! " << name << " is a really cool name.\n";
+    
+    int st_ID;
+    cout << "Fahsai: I think you are an Engineering student. What is your student ID?\n";
+    cout << name << ": ";
+    cin >> st_ID;
+    cin.ignore();
+    cout << "Fahsai: I think you may be GEAR "<< int(st_ID/1e7) - 12 << ". I have a free movie ticket for you.";
+    cout << "Fahsai: Let's go to the cinema together!!!\n" ;
+    
+    string movie;
+    cout << "Fahsai: What movie do you want to watch?\n" ;
+    cout << name << ": ";
+    getline(cin,movie); 
+
+    string day;
+    cout << "Fahsai: So....which day are you free to go with me?\n";
+    cout << name << ": ";    
+    getline(cin,day);
+    cout << "Fahsai: " << day << "....that is OK!!! I'm looking forward to watching " << movie <<" with you.\n" ;
+
+    string ans;
+    cout << name << ": ";
+    getline(cin,ans);
+
+    cout << "Fahsai: 555+ see you "<< day <<". Bye Bye \\(^ ^)/";
+}
